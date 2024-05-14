0d4a966cf67b4b6fab4add5d5ac1d52b1d60d875
Jay_local <go.jhson@gmail.com>
2024-05-14 22:19:04 +0900

    Add dec to base

--- module01/my_solutions/dec_to_base.go
+++ module01/my_solutions/dec_to_base.go
@@ -6,9 +6,18 @@ package module01
 //
 // Eg:
 //
-//   DecToBase(14, 16) => "E"
-//   DecToBase(14, 2) => "1110"
-//
+//	DecToBase(14, 16) => "E"
+//	DecToBase(14, 2) => "1110"
 func DecToBase(dec, base int) string {
-	return ""
+	result := ""
+	hexL := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"}
+
+	for {
+		result = hexL[dec%base] + result
+		dec = dec / base
+		if dec == 0 {
+			break
+		}
+	}
+	return result
 }
