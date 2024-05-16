ddfab4d82c70c4588a935783323cb74a184c4f4b
J Son <go.jhson@gmail.com>
2024-05-16 17:38:06 +0900

    Add base to base

--- module01/my_solutions/base_to_base.go
+++ module01/my_solutions/base_to_base.go
@@ -1,13 +1,29 @@
 package module01
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // BaseToBase takes in a number, the base it is currently
 // in, and the base you want it to be converted to. It then
 // returns a string representing the number in the new base.
 //
 // Eg:
 //
-//   BaseToBase("E", 16, 2) => "1110"
-//
+//	BaseToBase("E", 16, 2) => "1110"
 func BaseToBase(value string, base, newBase int) string {
-	return ""
+	switch {
+	case newBase == 10:
+		return fmt.Sprint(BaseToDec(value, base))
+	case base == 10:
+		i, err := strconv.Atoi(value)
+		if err != nil {
+			panic("Invalid Dev")
+		}
+		return DecToBase(i, newBase)
+	default:
+		f := BaseToDec(value, base)
+		return DecToBase(f, newBase)
+	}
 }
